f186fbfac27ac97ee006547ca79a8f336b41a23f
Yunu Choi <yunu.c.choi@gmail.com>
2024-12-20 17:04:17 -0800

    Basics for player (and board) is done

---
+++ pkg/board.java
@@ -0,0 +1,30 @@
+package pkg;
+
+public class Board {
+    private Deck dealer; 
+    private double pool;
+
+    public Board() {
+        dealer = new Deck();
+        pool = 0.0;
+    }
+
+    public Board(int decks) {
+        dealer = new Deck(decks);
+        pool = 0.0;
+    }
+
+    public void bet(double bet) {
+        pool += bet;
+    }
+
+    public double prizeMoney() {
+        double temp = pool;
+        pool = 0.0;
+        return temp;
+    } 
+
+    public Deck usingDeck() {
+        return this.dealer;
+    }
+}
