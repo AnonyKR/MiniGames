2406c09a700f4c8e166935eb57517977317c7257
AnonyKR <yunu.c.choi@gmail.com>
2024-12-31 21:09:36 -0800

    Some edit

--- starter.java
+++ starter.java
@@ -1,12 +1,9 @@
-import pkg.*;
-import pkg.TRPG.Trpg;
-
-import java.util.*;
+import pkg.TRPG.*;
 
 class starter {
 	public static void main(String args[]) {
         int first=(int)(Math.random()*6+1);
         int second=(int)(Math.random()*6+1);
-        System.out.println(Trpg);        
+        System.out.println(Trpg.rollDice(first, second));        
 	}
 }
