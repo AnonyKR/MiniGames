1ae5f40c7078a704c2d204358832697218d3133b
AnonyKR <yunu.c.choi@gmail.com>
2025-01-24 22:44:22 -0800

    finished (may need to edit the directory path)
    ChatGPT is amazing for getting help

--- pkg/LetterToWord/listWord.java
+++ pkg/LetterToWord/listWord.java
@@ -1,5 +1,146 @@
 package pkg.LetterToWord;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class listWord {
-    
+    private String[][] superList;
+    private ArrayList<String> toCheck;
+    private ArrayList<String> comment;
+    private Scanner sc;
+    private static HashSet<String> dictionary = new HashSet<>();
+
+
+    private static void loadAllWordFiles(String directoryPath) {
+        File directory = new File(directoryPath);
+        if (!directory.exists() || !directory.isDirectory()) {
+            System.err.println("Invalid SCOWL directory path: " + directoryPath);
+            return;
+        }
+
+        // Recursively load all files in the directory
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    loadWordsFromFile(file);
+                } else if (file.isDirectory()) {
+                    // Process subdirectories recursively
+                    loadAllWordFiles(file.getAbsolutePath());
+                }
+            }
+        }
+    }
+
+    private static void loadWordsFromFile(File file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                dictionary.add(line.trim().toLowerCase());
+            }
+            System.out.println("Loaded words from file: " + file.getName());
+        } catch (IOException e) {
+            System.err.println("Error reading file " + file.getName() + ": " + e.getMessage());
+        }
+    }
+
+    public listWord() {
+        toCheck = new ArrayList<>();
+        comment = new ArrayList<>();
+        String scowlDirectoryPath = "C://Users/Yunu Choi/Downloads/scowl-2020.12.07/final"; // Adjust this to your SCOWL directory path
+        loadAllWordFiles(scowlDirectoryPath);
+        sc = new Scanner(System.in);
+        superList = new String[0][0];
+    }
+
+    public listWord(int letters, int letterTypes) {
+        toCheck = new ArrayList<>();
+        comment = new ArrayList<>();
+        String scowlDirectoryPath = "C://Users/Yunu Choi/Downloads/scowl-2020.12.07/final"; // Adjust this to your SCOWL directory path
+        loadAllWordFiles(scowlDirectoryPath);
+        sc = new Scanner(System.in);
+        superList = new String[letters][letterTypes];
+    }
+
+    public boolean fillLetter(String[] letterTypes, int index) {
+        if (letterTypes.length == superList[0].length) {
+            superList[index] = letterTypes;
+            return true;
+        }
+        return false;
+    }
+
+    public void checkAll() {
+        int[] order = new int[superList.length];
+        ArrayList<Integer> toAdd = new ArrayList<>();
+        for (int i = 0; i < superList.length; i++) {
+            toAdd.add(i);
+        }
+        check(order, toAdd);
+    }
+
+    public void check(int[] order, ArrayList<Integer> toAdd) {
+        if(!toAdd.isEmpty()) {
+            for (int obj : toAdd) {
+                ArrayList<Integer> copy = (ArrayList<Integer>)toAdd.clone();
+                int[] orderCopy = order;
+                orderCopy[order.length - toAdd.size()] = obj;
+                copy.remove(copy.indexOf(obj));
+                check(orderCopy, copy);
+            }
+        } else {
+            checkOrder(order);
+        }
+    }
+
+    public void checkOrder(int[] order) {
+        int[] i = new int[order.length];
+        int max = superList[0].length;
+        String before = "";
+        while(true) {
+            String checkStr = "";
+            for(int n = 0; n < i.length; n++) {
+                checkStr += superList[order[n]][i[n]];
+            }
+            if (!checkStr.equals(before) && listWord.checkWord(checkStr)) {
+                before = checkStr;
+                System.out.print(checkStr + ": ");
+                String response = sc.nextLine();
+                if(!response.isEmpty()) {
+                    toCheck.add(checkStr);
+                    comment.add(response);
+                }
+            }
+            //Adding
+            int k = i.length - 1;
+            while (true) { 
+                if(i[k] == max - 1) {
+                    i[k] = 0;
+                    if (k == 0) {
+                        return;
+                    }
+                    k--;
+                } else {
+                    i[k] += 1;
+                    break;
+                }
+            }
+        }
+    }
+
+    public static boolean checkWord(String word) {
+        return dictionary.contains(word.toLowerCase());
+    }
+
+    public void printSave() {
+        for (int i = 0; i < toCheck.size(); i++) {
+            System.out.println(toCheck.get(i));
+            System.out.println("Comment: " + comment.get(i) + "\n");
+        }
+    }
 }
--- starter.java
+++ starter.java
@@ -1,90 +1,107 @@
-import java.util.Scanner;
-import pkg.TRPG.*;
+import pkg.LetterToWord.*;
 
 class starter {
-	public static void main(String args[]) {
-        
-        //System.out.println(Trpg.rollDice(first, second));
-        
-        //setting up character
-        Scanner sc=new Scanner(System.in);
-        //stats
-        int HP=0;
-        int MP=0;
-        int dexterity=0;
-        int agility=0;
-        int strength=0;
-        int intelligence=0;
-        //rolls
-		String[] jobs = {"archer", "warrior", "knight"}; //The roles reference is too frequent => make it to array
-		/* 
-        String a=new String("archer");
-        String b=new String("Archer");
-        String c=new String("Warrior");
-        String d=new String("warrior");
-        String e=new String("Knight");
-        String f=new String("knight");
-		*/
-        System.out.println("We are going to start by choosing job");
-        System.out.println("What job do you want?");
-		String job = "placeholder";
-		boolean validJob = false;
-        while(!validJob /*true*/){ //The loop conditions is changed in order to leave more easily
-			if (!job.equals("placeholder")) {
-				System.out.println("Choose your job again"); //This condition ensure that "again" sector only runs after first iteration
-			}
-			System.out.print("Choose from archer, warrior, or a knight: ");
-        	job = sc.nextLine(); //These lines need to be inside the loop if you want to ask again
-			/*
-                if(a.equals(job)||b.equals(job)){
-                        job="archer";
-                        System.out.println("You chose a archer");
-                        break;
-                }
-                else if(c.equals(job)||d.equals(job)){
-                        job="warrior";
-                        System.out.println("You chose a warrior");
-                        break;
-                }
-                else if(e.equals(job)||f.equals(job)){
-                        job="knight";
-                        System.out.println("You chose a knight");
-                        break;
-        
-                }
-                else{
-                        System.out.println("Choose your job again");
-                }
-			*/ //These lines only check if it matches with the given role, if that is a case, it can be changed into this form (since jobs are now in array)
-			for (int i = 0; i < jobs.length; i++) {
-				if (jobs[i].equals(job.toLowerCase())) {
-					validJob = true;
-					job = jobs[i];
-					System.out.println("You chose a " + jobs[i]);
-					break;
-				}
-			}
+        public static void main(String args[]) {
+                listWord testAll = new listWord(5, 4);
+                String[] anList = {"a", "a", "an", "an"};
+                String[] bnList = {"l", "la", "r", "ra"};
+                String[] cnList = {"s", "se", "n", "ne"};
+                String[] dnList = {"f", "fr", "p", "pr"};
+                String[] enList = {"d", "d", "do", "do"};
+                testAll.fillLetter(anList, 0);
+                testAll.fillLetter(bnList, 1);
+                testAll.fillLetter(cnList, 2);
+                testAll.fillLetter(dnList, 3);
+                testAll.fillLetter(enList, 4);
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n");
+                testAll.checkAll();
+                System.out.println();
+                testAll.printSave();
         }
+
+	// public static void main(String args[]) {
         
-        System.out.println("Now it's time to get your stat");
-        System.out.println("You can start with each stat of 14 points at most");
-        System.out.println("Roll the dice say 'roll' ");
-        String roll_cmd=sc.nextLine();
-        int roll_num=0;
-        if(job.equals(jobs[0])){ //Use equals method not random name that's not even a string... (also array can be referenced)
-                dexterity=2;
-                if(roll_cmd.equals("roll")){
-                        dexterity= Trpg.rollDice();
-                        System.out.println();
+        // //System.out.println(Trpg.rollDice(first, second));
+        
+        // //setting up character
+        // Scanner sc=new Scanner(System.in);
+        // //stats
+        // int HP=0;
+        // int MP=0;
+        // int dexterity=0;
+        // int agility=0;
+        // int strength=0;
+        // int intelligence=0;
+        // //rolls
+	// 	String[] jobs = {"archer", "warrior", "knight"}; //The roles reference is too frequent => make it to array
+	// 	/* 
+        // String a=new String("archer");
+        // String b=new String("Archer");
+        // String c=new String("Warrior");
+        // String d=new String("warrior");
+        // String e=new String("Knight");
+        // String f=new String("knight");
+	// 	*/
+        // System.out.println("We are going to start by choosing job");
+        // System.out.println("What job do you want?");
+	// 	String job = "placeholder";
+	// 	boolean validJob = false;
+        // while(!validJob /*true*/){ //The loop conditions is changed in order to leave more easily
+	// 		if (!job.equals("placeholder")) {
+	// 			System.out.println("Choose your job again"); //This condition ensure that "again" sector only runs after first iteration
+	// 		}
+	// 		System.out.print("Choose from archer, warrior, or a knight: ");
+        // 	job = sc.nextLine(); //These lines need to be inside the loop if you want to ask again
+	// 		/*
+        //         if(a.equals(job)||b.equals(job)){
+        //                 job="archer";
+        //                 System.out.println("You chose a archer");
+        //                 break;
+        //         }
+        //         else if(c.equals(job)||d.equals(job)){
+        //                 job="warrior";
+        //                 System.out.println("You chose a warrior");
+        //                 break;
+        //         }
+        //         else if(e.equals(job)||f.equals(job)){
+        //                 job="knight";
+        //                 System.out.println("You chose a knight");
+        //                 break;
+        
+        //         }
+        //         else{
+        //                 System.out.println("Choose your job again");
+        //         }
+	// 		*/ //These lines only check if it matches with the given role, if that is a case, it can be changed into this form (since jobs are now in array)
+	// 		for (int i = 0; i < jobs.length; i++) {
+	// 			if (jobs[i].equals(job.toLowerCase())) {
+	// 				validJob = true;
+	// 				job = jobs[i];
+	// 				System.out.println("You chose a " + jobs[i]);
+	// 				break;
+	// 			}
+	// 		}
+        // }
+        
+        // System.out.println("Now it's time to get your stat");
+        // System.out.println("You can start with each stat of 14 points at most");
+        // System.out.println("Roll the dice say 'roll' ");
+        // String roll_cmd=sc.nextLine();
+        // int roll_num=0;
+        // if(job.equals(jobs[0])){ //Use equals method not random name that's not even a string... (also array can be referenced)
+        //         dexterity=2;
+        //         if(roll_cmd.equals("roll")){
+        //                 dexterity= Trpg.rollDice();
+        //                 System.out.println();
 
-                }
+        //         }
 
-        }
-        else if(job.equals(jobs[1])){
+        // }
+        // else if(job.equals(jobs[1])){
 
-        }
-        else {
+        // }
+        // else {
 
-		}
-	}
+	// 	}
+	// }
 }
